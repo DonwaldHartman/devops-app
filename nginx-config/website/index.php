8a441eb3dd6776f0383a036fed2508f25bf06203
Donwald <donwald.hartman@gmail.com>
2023-02-06 22:47:51 +0000

    index.php updated

--- nginx-config/website/index.php
+++ nginx-config/website/index.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./styles.css">
+    <link rel="stylesheet" href="styles.css">
     <title>Document</title>
 </head>
 <body>
     <div id='app' class='v-align' >
         <div class='hero-btn'>
           <button onclick="startAnimation()" class='btn'>
-            Big Friendly Button
+            Bigger Friendlier Button
           </button>
           <span class='particles-circle'></span>
           <span class='particles-circle'></span>
@@ -52,7 +52,7 @@
 
       </div>
       <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
- <script src="./script.js"></script>
+ <script src="script.js"></script>
 </body>
 
 </html>
